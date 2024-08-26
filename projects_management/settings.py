158273241a7965985818e311fab450199f29915c
ThugyBoy <132939797+ThugyBoy@users.noreply.github.com>
2024-08-26 22:26:16 +0300

    Login & logout

--- projects_management/settings.py
+++ projects_management/settings.py
@@ -131,6 +131,10 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/accounts/login'
+
 INTERNAL_IPS = [
 
     "127.0.0.1",
