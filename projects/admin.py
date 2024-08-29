2b39c55677b01132c47dc49667858fd0997a189a
ThugyBoy <132939797+ThugyBoy@users.noreply.github.com>
2024-08-29 11:14:31 +0300

    Admin Panel

--- projects/admin.py
+++ projects/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from . import models
 
+
+admin.site.register(models.Category)
+admin.site.register(models.Project)
+admin.site.register(models.Task)
 # Register your models here.
--- projects_management/urls.py
+++ projects_management/urls.py
@@ -18,6 +18,12 @@ from django.contrib import admin
 from django.urls import path, include
 import debug_toolbar
 
+from django.utils.translation import gettext as _
+
+
+admin.site.site_header = _('Projects Management')
+admin.site.site_title = _('Projects Management')
+
 urlpatterns = [
     path('__debug__/', include('debug_toolbar.urls')),
     path('', include('projects.urls')),
